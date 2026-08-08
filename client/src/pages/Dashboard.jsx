279519420e9f902f703215e543b2c0228ae78fa8
unknown <202008867+Ace-spoe@users.noreply.github.com>
2026-08-08 12:42:06 +0300

    add edit and fix delete/create refinements on Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -9,6 +9,23 @@ const Dashboard = () => {
   const [ formData , setFormData ] = useState({
       name : '' , frequency : ''
     })
+  const [toggleForEdit, setToggleForEdit] = useState('')
+  const [ editFormData , setEditFormData ] = useState({
+      name : '' , frequency : ''
+    })
+
+  function handleToggleForEdit( id ) {
+    setToggleForEdit(prev => {
+      if(prev === id) {
+        return ''
+      } 
+      
+      else{ 
+        return id
+      }
+    })
+  }
+
   const getHabits = async () => {
     try {
       
@@ -39,11 +56,9 @@ const Dashboard = () => {
     
   }
 
-
   useEffect(() => {
     getHabits()
   }, [])
-
 
   const handleCreate = async (e) => {
     e.preventDefault()
@@ -79,8 +94,93 @@ const Dashboard = () => {
     }
   }
 
+  const handleDelete = async (id) => {
+
+    try {
+       const res = await fetch (`http://localhost:3000/api/habits/${id}`, {
+          method : 'DELETE' ,
+          headers : {
+            'Content-Type' : 'application/json'
+          },
+          credentials : 'include',
+    })
+    
+    if (!res.ok) {
+          setErr('Some thing went wrong');
+          return console.log('Some thing went wrong')
+    }
+
+    console.log('Deleted Successfuly')
+    getHabits()
+
+    } catch (err) {
+      setErr('Unable to connect to the internet')
+    }
+  }
+
+  const handleEdit = async (e , id) => {
+    e.preventDefault()
+    
+    
+    try {
+      const res = await fetch (`http://localhost:3000/api/habits/${id}`, {
+          method : 'PUT' ,
+          headers : {
+            'Content-Type' : 'application/json'
+          },
+          credentials : 'include',
+          body : JSON.stringify(editFormData)
+    })
+    
+    const response = await res.json()
+    
+    if (!res.ok) {
+      setLoading(false)
+      setErr(response.message);
+      return console.log('Some thing went wrong')
+    }
+    console.log('Editied succesfully')
+    getHabits()
+
+    } catch (err) {
+      setErr('Unable to connect to the internet')
+    }
+  }
+
   const habitsList =  habit.map((item) => (
-    <p key = {item._id}> {item.name} </p>
+    <div key = {item._id}>
+      <p> {item.name} </p> 
+      <input type="button" value="Delete" onClick={ () => handleDelete(item._id) } />
+
+      <input type="button" value="Edit" 
+      onClick={ () => {
+        handleToggleForEdit(item._id)
+        setEditFormData({ name: item.name, frequency: item.frequency })
+        } } />
+      {toggleForEdit === item._id  && 
+      
+      <form onSubmit={(e) => handleEdit(e , item._id)}>
+      <input type="text" 
+      required placeholder='new name'
+      value = {editFormData.name} 
+      onChange={e => setEditFormData({...editFormData , name : e.target.value })}
+      />
+      <select 
+      value={editFormData.frequency}
+      onChange={e => setEditFormData({...editFormData, frequency: e.target.value})}>
+        <option value="">Select new Frequency</option>
+        <option value="daily">daily</option>
+        <option value="weekly">weekly</option>
+        <option value="monthly">monthly</option>
+      </select>
+
+      <input type = 'submit'/>
+      {err && <p>{err}</p>}
+      </form>
+      }
+    </div>
+    
+    
   ))
 
   return (
@@ -90,9 +190,9 @@ const Dashboard = () => {
         {habitsList}
         {err && <p>{err}</p>}
     </div>
-    {!toggle && 
+    
     <input onClick={() => setToggle( prev => !prev)}
-      type = 'submit' value='Add habit'/>}
+      type = 'submit' value='Add habit'/>
     {toggle && 
     <form onSubmit={handleCreate}>
       <input type="text" 
