4ae0d88a0b0717ebcb533e98cb718fe9eeb48a77
unknown <202008867+Ace-spoe@users.noreply.github.com>
2026-08-17 23:05:02 +0300

    Fix : changed title , added favicon and updating loading UI

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -7,7 +7,10 @@ const ProtectedRoute = ({children}) => {
     const { user , loading } = useAuth()
 
     if (loading) {
-    return <p>Loading...</p>
+    return <div className="flex flex-col items-center justify-center py-16 gap-3">
+            <div className="w-8 h-8 border-3 border-teal-400 border-t-transparent rounded-full animate-spin"></div>
+            <p className="text-xs text-slate-400 font-medium">Loading...</p>
+          </div>
    }
    
     if(!user) {
--- client/src/components/PublicRoute.jsx
+++ client/src/components/PublicRoute.jsx
@@ -5,7 +5,11 @@ const PublicRoute = ({ children }) => {
   const { user, loading } = useAuth()
 
   if (loading) {
-    return <p>Loading...</p>
+    return <div className="flex flex-col items-center justify-center py-16 gap-3">
+            <div className="w-8 h-8 border-3 border-teal-400 border-t-transparent rounded-full animate-spin"></div>
+            <p className="text-xs text-slate-400 font-medium">Fetching your habits...</p>
+          </div>
+      
   }
 
   if (user) {
